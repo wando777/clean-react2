910a645ce4a137126a4e20c80b91a6280d3b2cd9
wando777 <wanderson.eq2011@gmail.com>
2024-02-21 09:43:30 -0300

    chore: add faker

--- tests/domain/usecases/remote-authenticaton.spec.ts
+++ tests/domain/usecases/remote-authenticaton.spec.ts
@@ -1,26 +1,33 @@
 import { type HttpClient } from '@/infra/gateways/protocols'
 import { RemoteAuthentication } from '@/domain/usecases'
 import { mock, type MockProxy } from 'jest-mock-extended'
+import { faker } from '@faker-js/faker'
+import { type Authentication } from '@/domain/usecases/protocols'
 
 describe('RemoteAuthentication', () => {
   let httpClient: MockProxy<HttpClient>
   let url: string
   let sut: RemoteAuthentication
+  let credentials: Authentication.Input
 
   beforeAll(() => {
     httpClient = mock()
-    url = 'any_url'
+    url = faker.internet.url()
+    credentials = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
   })
   beforeEach(() => {
     sut = new RemoteAuthentication(url, httpClient)
   })
 
   it('Should call HttpClient with correct params', async () => {
-    await sut.auth({ email: 'any_email', password: 'any_password' })
+    await sut.auth(credentials)
 
     expect(httpClient.post).toHaveBeenCalledWith({
       url,
-      data: { email: 'any_email', password: 'any_password' }
+      data: credentials
     })
   })
 })
